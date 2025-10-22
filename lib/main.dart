4298ba596402fc1ad2c425ad9acbd44b3ad8e3cb
ChrystallaT1 <chrystallatambouri@hotmail.com>
2025-10-22 16:37:32 +0100

    Define the Main App Widget: App

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
